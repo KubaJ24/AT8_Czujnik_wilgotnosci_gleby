03fa3a57329f772a29906b8ba63186ca4f2a229c
KubaJ24 <j.jarosz@onet.pl>
2024-02-11 20:40:47 +0100

    Dodano funkcję obsługi przerwania ADC

--- AT8_Czujnik_wilgotnosci_gleby/main.c
+++ AT8_Czujnik_wilgotnosci_gleby/main.c
@@ -7,11 +7,23 @@
 
 #include "main.h"
 
+volatile uint16_t ADC_Conv_result = 0;
+
+/*
+ Funkcja obs³ugi przewania ADC - koniec przetwarzania
+*/
+void ADC_vect(void){
+	ADC_Conv_result = my_ADC_Result();
+}
 
 int main(void)
 {
     
+	my_ADC_Setup();
+	
 	sei();					// Global Interrupt Enable
+	
+	my_ADC_Start();
 	
     while(1){
 		
